4c7600a494f09f90ae0a437b26620572af1bf393
Ismael Cardenas <cardenas.ismael@gmail.com>
2021-09-18 17:36:56 -0700

    First Draft

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//Collapse menu after click
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
+
+//deprecated
+// $('.navbar-nav>li>a').click(function(){
+//     $('.navbar-collapse').collapse('hide');
+// });
+
